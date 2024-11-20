71b77ca3374b91b92c3bf5371421ed491ee22090
Amna1993 <amnarazzaqawan@gmail.com>
2024-11-20 01:19:35 -0500

    Menu Modification

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
-import Gallery from './components/Gallery/Gallery';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Menu from './components/Menu/Menu';
@@ -11,7 +10,6 @@ function App() {
       <Header />
       <Hero />
       <Menu />
-      <Gallery />
       <Footer />
     </div>
   );
